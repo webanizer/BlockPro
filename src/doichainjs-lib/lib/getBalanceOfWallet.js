652f5ec4592c166664b192c809f0cfd77fd1673a
lena <lena@stallinger.de>
2021-10-28 16:50:10 +0200

    #4 wenn alle Addressen benutzt wurden, neue generieren und speichern

--- src/doichainjs-lib/lib/getBalanceOfWallet.js
+++ src/doichainjs-lib/lib/getBalanceOfWallet.js
@@ -1,3 +1,4 @@
+import { address } from "bitcoinjs-lib";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 import { getAddress } from "./getAddress.js";
@@ -26,7 +27,7 @@ export const getBalanceOfWallet = async (xpub, purpose, coinType, account, o_opt
     if (pathsAndAddresses == undefined){
         pathsAndAddresses =   []
         let change = 0 // erste neue Addresse ist Empfangsadresse
-        let index = 0  // index der ersten neuen Empfangsadresse
+        let index  = 0 // index der ersten neuen Empfangsadresse
         newDerivationPath = `${purpose}/${coinType}/${account}/${change}/${index}`
         newAddress = getAddress(xpub.derivePath(newDerivationPath).publicKey, options, addrType, newDerivationPath)
         await saveAddress(purpose, newDerivationPath, newAddress)
@@ -52,7 +53,7 @@ export const getBalanceOfWallet = async (xpub, purpose, coinType, account, o_opt
     let retAddresses = addressesRet.addresses
     addresses = []
 
-    // TO DO nur eine Address in addresses und derivation path fehlt
+    // addresses von Electrum um derivationPath ergänzen
     for (let i = 0; i < retAddresses.length; i++) {
         for (let j = 0; j < pathsAndAddresses.length; j++) {
             if (retAddresses[i].address == pathsAndAddresses[j].readAddress) {
@@ -64,7 +65,57 @@ export const getBalanceOfWallet = async (xpub, purpose, coinType, account, o_opt
         }
     }
 
-        balance = addressesRet.transactionCount > 0 ? Number(addressesRet.balance).toFixed(8) : 0
 
-        return { balance: balance, addresses: addresses, transactionCount: addressesRet.transactionCount }
+    // if all receiving addresses and all change addresses have transactions, create a new address for each type
+    let unusedReceivingAddresses = []
+    let unusedChangeAddresses = []
+
+    // TO DO: Diese zwei in arrays ändern und unten bei last derivationPath den höchsten Index herausfinden
+    let lastChangeDerivationPath
+    let lastReceiveDerivationPath 
+
+    // check if there are still unused receiving or change addresses
+    for (let i = 0; i < addresses.length; i++) {
+        let address = addresses[i]
+        let derivationPath = address.derivationPath
+        let change = (derivationPath.split("/")[4] == 1)
+        if (!change  && address.transactions.length == 0){
+            unusedReceivingAddresses.push(addresses[i])
+        }else if (change && address.transactions.length == 0){
+            unusedChangeAddresses.push(addresses[i])
+        }
+        change ? lastChangeDerivationPath = derivationPath : lastReceiveDerivationPath = derivationPath
+    }
+
+    // if there are no unused addresses left create new ones
+    if (unusedReceivingAddresses.length == 0){
+        let previousIndex  = lastReceiveDerivationPath.split("/")[5] 
+        var lastIndex = lastReceiveDerivationPath.lastIndexOf('/');
+        newDerivationPath = lastReceiveDerivationPath.substr(0, lastIndex) + "/" + ++previousIndex      
+        newAddress = getAddress(xpub.derivePath(newDerivationPath).publicKey, options, addrType, newDerivationPath)
+        await saveAddress(purpose, newDerivationPath, newAddress)
+        let address = {}
+        address["address"] = newAddress
+        address["balance"] = 0
+        address["derivationPath"] = newDerivationPath
+        address["transactions"] = []
+        addresses.push(address)
+
+    } else if (unusedChangeAddresses.length == 0){
+        let previousIndex  = lastChangeDerivationPath.split("/")[5] 
+        var lastIndex = lastChangeDerivationPath.lastIndexOf('/');
+        newDerivationPath = lastChangeDerivationPath.substr(0, lastIndex) + "/" + ++previousIndex      
+        newAddress = getAddress(xpub.derivePath(newDerivationPath).publicKey, options, addrType, newDerivationPath)
+        await saveAddress(purpose, newDerivationPath, newAddress)
+        let address = {}
+        address["address"] = newAddress
+        address["balance"] = 0
+        address["derivationPath"] = newDerivationPath
+        address["transactions"] = []
+        addresses.push(address)
+    }
+
+    balance = addressesRet.transactionCount > 0 ? Number(addressesRet.balance).toFixed(8) : 0
+
+    return { balance: balance, addresses: addresses, transactionCount: addressesRet.transactionCount }
     }
